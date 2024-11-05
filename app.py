3730f20552d27e89b051a74c3071dea50d88cb57
LancemDev <munyaolance1@gmail.com>
2024-11-05 14:21:19 +0300

    Feat: Define block textures and initialize the player character with FirstPersonController

--- app.py
+++ app.py
@@ -7,3 +7,19 @@ import time
 # Set up the game and Perlin noise for terrain generation
 app = Ursina()
 noise = PerlinNoise(octaves=3, seed=random.randint(1, 1000))
+
+
+# Define game variables and textures
+selected_block = "grass"
+block_textures = {
+    "grass": load_texture("assets/textures/groundEarth.png"),
+    "dirt": load_texture("assets/textures/groundMud.png"),
+    "stone": load_texture("assets/textures/wallStone.png"),
+    "bedrock": load_texture("assets/textures/stone07.png")
+}
+
+# Initialize player
+player = FirstPersonController(
+    mouse_sensitivity=Vec2(100, 100),
+    position=(0, 5, 0)
+)
