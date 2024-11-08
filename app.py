b7e926dd323b597f1129827717d4e09752693ec4
Edith Haggai <131600246+Edith-Haggai@users.noreply.github.com>
2024-11-08 08:21:52 +0300

    Definition of block types.py

--- app.py
+++ app.py
@@ -66,3 +66,36 @@ def spawn_shooting_star():
     invoke(destroy, star, delay=2)
 
 invoke(spawn_shooting_star, delay=random.uniform(0, 5))
+
+# Input handling
+def input(key):
+    global selected_block
+    if key == "left mouse down":
+        hit_info = raycast(camera.world_position, camera.forward, distance=10)
+        if hit_info.hit:
+            block = Block(hit_info.entity.position + hit_info.normal, selected_block)
+    if key == "right mouse down" and mouse.hovered_entity:
+        if not mouse.hovered_entity.block_type == "bedrock":
+            destroy(mouse.hovered_entity)
+    if key == "1":
+        selected_block = "grass"
+    if key == "2":
+        selected_block = "dirt"
+    if key == "3":
+        selected_block = "stone"
+    if key == "f":
+        window.fullscreen = not window.fullscreen
+    if key == "m":
+        mouse.visible = not mouse.visible
+        mouse.locked = not mouse.locked
+    if key == "escape":
+        application.quit()
+        print("Closing application safely")
+        
+instruction_text = Text(
+    text="Left Mouse: Place Block | Right Mouse: Delete Block | 1: Grass | 2: Dirt | 3: Stone | F: Toggle Fullscreen | m: Make mouse visible | Esc: Quit",
+    position=(0, -0.4),
+    origin=(0, 0),
+    scale=0.75,
+    background=True
+)
